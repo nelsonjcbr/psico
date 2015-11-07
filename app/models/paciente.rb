c8fc2e12e4a42297bd5b338a70f713159851e041
Nelson Coelho <nelson@inovadora.com.br>
2015-11-07 11:02:02 -0200

    Formato da data brasileiro

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -3,4 +3,9 @@ class Paciente < BancoLegado
   #:extend UpdatableView
   self.table_name = 'pacientes'
   self.primary_key = 'id'
+
+  def nome_e_id
+    self.nome + '(' + self.id.to_s + ')'
+  end
+
 end
